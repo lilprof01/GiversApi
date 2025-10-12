dc900a478a33fb7da3527a75b7ee8e9d6e434bb2
lilprof01 <adewumime@gmail.com>
2025-10-12 18:58:03 +0100

    Fix Firebase env config for Render

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,18 @@
 import admin from "firebase-admin";
 import path from "path";
 
-const serviceAccountPath = path.resolve(__dirname, "./serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(require(serviceAccountPath)),
-});
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  console.log("⚠️ Using local serviceAccountKey.json file");
+  const serviceAccount = require("./serviceAccountKey.json");
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} else {
+  console.log("✅ Using FIREBASE_SERVICE_ACCOUNT from environment");
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export const db = admin.firestore();
